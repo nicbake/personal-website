7143479ec8e19bfa151bd2b836d3794828a66e62
Nicholas Baker <nicholas.baker180@gmail.com>
2024-01-10 11:45:57 -0500

    Added some resume stuff

--- src/components/Table.js
+++ src/components/Table.js
@@ -16,8 +16,17 @@ const Table = () => {
         <tr>
           <td>
             <Navbar/>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h2>Work Experience</h2>
+            <p>it specialists — guelph independent living - guelph, on</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
             <p>Nicholas baker is a blank <a href='/personal-website/public/NB_Resume2022.pdf'>pdf</a></p>
-
           </td>
         </tr>
     </table>
