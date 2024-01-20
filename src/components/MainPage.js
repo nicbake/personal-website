be3d861672282b736caf9bd12dcbb81c5e96b780
Nicholas Baker <nicholas.baker180@gmail.com>
2024-01-20 15:06:41 -0500

    playing around

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,6 +5,29 @@ import Table from "./Table";
 const MainPage = () => {
   return (
     <div className='MainPage'>
+        <h1><a href="#">nicholas baker</a></h1>
+        <h2>I'm a passionate <span>software developer</span> from Guelph, ON</h2>
+        
+        <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link active" href="#header">Home</a></li>
+          <li><a class="nav-link" href="#about">About</a></li>
+          <li><a class="nav-link" href="#resume">Resume</a></li>
+          <li><a class="nav-link" href="#services">Services</a></li>
+          <li><a class="nav-link" href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+      </div>
+
+      
         <Table />
     </div>
   )
